8c1887344611956c97228db96f791025dd34325c
rsosseh <ray_sosseh@brown.edu>
2018-07-09 13:48:32 -0400

    url error

--- src/components/AddPlaylist.js
+++ src/components/AddPlaylist.js
@@ -11,7 +11,7 @@ class AddPlaylist extends Component{
 		if(this.props.show){
 			content.push([
 				<h1 className="fest_name">{this.props.fest_name}</h1>,
-				<a href="javascript:window.open('https://accounts.spotify.com/authorize?client_id=47c457c054214945af809dc6674f5cff&redirect_uri=http:%2F%2Fhttps://festiv-app.herokuapp.com/success&scope=user-read-private%20user-read-email%20playlist-modify-private&response_type=token')"><h1 className="add_to_title">Add Playlist</h1></a>
+				<a href="javascript:window.open('https://accounts.spotify.com/authorize?client_id=47c457c054214945af809dc6674f5cff&redirect_uri=http:%2F%2Ffestiv-app.herokuapp.com/success&scope=user-read-private%20user-read-email%20playlist-modify-private&response_type=token')"><h1 className="add_to_title">Add Playlist</h1></a>
 			])
 		}
 		return(
