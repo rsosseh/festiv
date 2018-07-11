5781591e43fed726abb8ff9d0195246b935c6f68
rsosseh <ray_sosseh@brown.edu>
2018-07-10 22:08:09 -0400

    list multiple artists if on track

--- src/components/PlaylistAdder.js
+++ src/components/PlaylistAdder.js
@@ -4,14 +4,24 @@ import { CSSTransitionGroup } from 'react-transition-group'
 let item_classes = ['track_item']
 
 class PlaylistAdder extends Component{
+
+
+
 	render(){
-		
+
 		let tracks = this.props.tracks.map( (track, i) => {
+			var artists_string = track.artists[0].name
+			if(track.artists.length > 1){
+				for (var i = 1; i < track.artists.length; i++){
+					artists_string += ', ' + track.artists[i].name
+				}
+			}
+
 			return <div className={item_classes.join(' ')} style={{"transitionDelay": `${ i * .05 }s` }}>
 					<a href={track.external_urls.spotify}><img src={track.album.images[1].url} /></a>
 					<div className="track_info">
 						<p>{track.name}</p>
-						<p>{track.artists[0].name}</p>
+						<p>{artists_string}</p>
 					</div>
 				</div>
 		})
