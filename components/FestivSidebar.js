259e93adc5a636b14fb88a6af3f05e766cba22fa
Ray Sosseh <raysosseh@Rays-MacBook-Pro-2.local>
2020-07-13 23:05:38 -0400

    add playlist link to callback

--- components/FestivSidebar.js
+++ components/FestivSidebar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 class FestivSidebar extends React.Component{
+    callSpotify = () => {
+        const spotifyEndpoint = "https://accounts.spotify.com/authorize?client_id=47c457c054214945af809dc6674f5cff&redirect_uri=http:%2F%2Ffestiv-app.herokuapp.com/success&scope=user-read-private%20user-read-email%20playlist-modify-private&response_type=token'";
+        window.open(spotifyEndpoint);
+    }
 	render(){
         const { festivalName } = this.props;
-        const spotifyEndpoint = "javascript:window.open('https://accounts.spotify.com/authorize?client_id=47c457c054214945af809dc6674f5cff&redirect_uri=http:%2F%2Ffestiv-app.herokuapp.com/success&scope=user-read-private%20user-read-email%20playlist-modify-private&response_type=token')";
 
 		return(
             <div className="festiv__sidebar">
                 <div className="add_to_tab">
                     <h1 className="fest_name">{festivalName}</h1>
-                    <a href={spotifyEndpoint}>
+                    <a onClick={this.callSpotify}>
                         <h1 className="add_to_title">Add Playlist</h1>
                     </a>
                 </div>
